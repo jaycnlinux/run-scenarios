8b749c1cc0a63ef6b5b5112d2b67e08a3f204d90
jaycnlinux <43997493+jaycnlinux@users.noreply.github.com>
2019-05-20 15:01:17 +0800

    Delete install_soft.sh

--- install_soft.sh
+++
@@ -1,9 +0,0 @@
-yum -y install unzip gcc gcc-c++ epel-release
-wget --no-check-certificate https://github.com/HewlettPackard/netperf/archive/netperf-2.7.0.zip -O netperf-2.7.0.zip
-unzip netperf-2.7.0.zip
-cd netperf-netperf-2.7.0/
-./configure && make && make install
-cd
-yum -y install memcached libmemcached
-yum -y install qperf
-yum -y install nginx
