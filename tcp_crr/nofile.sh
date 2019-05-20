f657c26db9eb1a55bb77a5cd737c339ddc6be164
jaycnlinux <43997493+jaycnlinux@users.noreply.github.com>
2019-05-20 15:01:52 +0800

    Delete nofile.sh

--- tcp_crr/nofile.sh
+++
@@ -1,4 +0,0 @@
-echo "fs.nr_open=50000000"  >> /etc/sysctl.conf
-echo "fs.file-max=50000000" >> /etc/sysctl.conf
-echo "* soft nofile 50000000" >> /etc/security/limits.conf
-echo "* hard nofile 50000000" >> /etc/security/limits.conf
